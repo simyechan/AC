1d7789974f9d404e411dd533ec9663d28d90b9c9
sim <simyechan87@gmail.com>
2024-02-14 18:35:52 +0900

    db 연결

--- server/server.js
+++ server/server.js
@@ -1,8 +1,52 @@
+require('dotenv').config({ path: '../.env' });
+
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+  });
+  
+  // MySQL 연결
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+  });
+  
+  // 저장 버튼 클릭 -> 정보를 db에 저장
+  app.post('/saveExpense', (req, res) => {
+    const { date, amount, description } = req.body;
+  
+    const query = 'INSERT INTO expenses (date, amount, description) VALUES (?, ?, ?)';
+    connection.query(query, [date, amount, description], (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error saving expense');
+      } else {
+        res.send('Expense saved successfully');
+      }
+    });
+  });
+  
+  // db에서 지출 정보 클라이언트에 응답
+  app.get('/expenses', (req, res) => {
+    connection.query('SELECT * FROM expenses', (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error fetching expenses');
+      } else {
+        res.json(results);
+      }
+    });
+  });
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, 'public')));
