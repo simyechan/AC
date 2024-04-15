ce0897798b974ead717ef6e71b35da904ba9bbb5
sim <simyechan87@gmail.com>
2024-04-15 11:30:36 +0900

    입출금 나누기

--- front/src/mainpage/main.js
+++ front/src/mainpage/main.js
@@ -11,36 +11,50 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         events: '/getAmount'
     });
-    
     calendar.render();
-    
+
+    fetch('/categorys')
+        .then(response => response.json())
+        .then(categories => {
+            const selectCategory = this.documentElement('expenseCategory');
+
+            categories.forEach(category => {
+                const option = document.createElement('option');
+                option.value = category.id;
+                option.textContent = category.name;
+                selectCategory.appendChild(option);    
+            });
+        })
+        .catch(error => {
+            console.error('카테고리를 가져오는 중 오류가 발생했습니다.', error);
+        });
+
     function saveExpense() {
         var date = document.getElementById('selectedDate').value;
         var amount = document.getElementById('expenseAmount').value;
+        var category = document.getElementById('expenseCategory').value;
         var description = document.getElementById('expenseDescription').value;
         
-        alert('날짜: ' + date + ', 금액: ' + amount + ', 설명: ' + description + '가 저장되었습니다.');
+        alert('날짜: ' + date + ', 금액: ' + amount + ', 설명: ' + description + ', 카테고리 ID: ' + category + '가 저장되었습니다.');
         calendar.refetchEvents();
         
         document.getElementById('expenseAmount').value = '';
+        document.getElementById('expenseCategory').value = '';
         document.getElementById('expenseDescription').value = '';
         document.getElementById('expenseForm').style.display = 'none';
     }
     
     // 저장 버튼에 클릭 이벤트를 추가합니다.
     document.getElementById('saveButton').addEventListener('click', saveExpense);
-})
-.catch(function(error) {
-    console.error('error 발생 : ', error);
+
+    function logingo() {
+        location.href = '../login/login.html';
+    };
+
+    function registrationgo() {
+        // registrationgo 함수의 구현 내용은 여기에 추가하세요
+    };
 });
-
-function logingo() {
-    location.href = '../login/login.html';
-};
-
-function registrationgo() {
-    // registrationgo 함수의 구현 내용은 여기에 추가하세요
-};
 
 
 // axios.defaults.baseURL = 'http://192.168.52.156:8080';
