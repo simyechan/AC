461789cd5839893199914fda1b951dff30cef092
sim <simyechan87@gmail.com>
2024-05-31 08:58:16 +0900

    인증 및 카테고리관련 수정

--- front/src/deposit/deposit.js
+++ front/src/deposit/deposit.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
   const depoInput = document.getElementById('depoInput');
   const explInput = document.getElementById('explInput');
   const categoryInput = document.getElementById('categoryInput');
+  const dateInput = document.getElementById('dateInput');
   const warnDepo = document.getElementById('warnDepo');
   const warnExpl = document.getElementById('warnExpl');
   const warnCategory = document.getElementById('warnCategory');
+  const warnDate = document.getElementById('warnDate');
 
   depositBtn.addEventListener('click', function (event) {
     event.preventDefault();
@@ -22,60 +24,67 @@ document.addEventListener('DOMContentLoaded', function () {
       warnExpl.classList.add('transparent');
     }
 
-    if (categoryInput.value.toLowerCase() === '') {
-      warnCategory.classList.remove('transparent');
+    if (dateInput.value.trim() === '') {
+      warnDate.classList.remove('transparent');
     } else {
-      warnCategory.classList.add('transparent');
+      warnDate.classList.add('transparent');
     }
 
     const amount = parseFloat(depoInput.value.trim());
     const explanation = explInput.value.trim();
+    const date = dateInput.value.trim();
     const category = categoryInput.value.trim();
 
     if (!isNaN(amount) && amount >= 0) {
       const selectedCategory = category.trim() !== '' ? category : '기타';
+      const token = localStorage.getItem('accessTkn');
       axios.post('http://localhost:8000/income/deposit', {
         amount: amount,
         explanation: explanation,
+        date: date,
         category: selectedCategory
+      }, {
+        headers: {
+          'authorization': `Bearer ${token}`
+        }
       })
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error('Error: ', error);
-      });
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.error('Error: ', error);
+        });
     } else {
       console.error('Invalid amount: ', depoInput.value);
     }
   });
 
-  async function fetchCategories() {
-    try {
-      const response = await axios.get('http://localhost:8000/common/categorys');
-      return response.data;
-    } catch (error) {
-      console.error(error);
-      return [];
-    }
-  }
-
   async function searchCategories() {
     const input = categoryInput.value.toLowerCase();
     const categoryList = document.getElementById('categoryList');
-    
+  
+    async function fetchCategories() {
+      try {
+        const response = await axios.get('http://localhost:8000/common/categorys');
+        return response.data;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
+    }
+  
     try {
       const categories = await fetchCategories();
       const filterCategory = categories.filter(category => category.toLowerCase().includes(input));
-
+  
       categoryList.innerHTML = '';
-
+  
       filterCategory.forEach(category => {
         const list = document.createElement('li');
         list.textContent = category;
         list.addEventListener('click', () => {
-          categoryInput.value = category;
           categoryList.innerHTML = '';
+          categoryInput.value = category;
         });
         categoryList.appendChild(list);
       });
